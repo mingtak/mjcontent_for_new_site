d40fb2fcf732df3672ce34c9957356b4fd0d174a
andy fang <andy@mingtak.com.tw>
2016-12-06 13:35:25 +0000

    update

---
+++ src/mingjing/content/browser/views.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from plone import api
+
+
+class CoverView(BrowserView):
+
+    template = ViewPageTemplateFile("template/cover_view.pt")
+
+    def __call__(self):
+        context = self.context
+        request = self.request
+        portal = api.portal.get()
+
+        return self.template()
