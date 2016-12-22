3cd5ac41db59975304503bc77fad98c17323754f
andy fang <andy@mingtak.com.tw>
2016-12-22 23:01:19 +0000

    add rwd_menu

--- src/mingjing/content/viewlet/viewlet.py
+++ src/mingjing/content/viewlet/viewlet.py
@@ -3,5 +3,9 @@ from plone.app.layout.viewlets import common as base
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 
+class RwdMenu(base.ViewletBase):
+    """  """
+
+
 class ScriptToFooter(base.ViewletBase):
     """  """
