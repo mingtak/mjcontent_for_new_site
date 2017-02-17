fba4a67d37019d7e85d9b70ba2cb528bc3a711e6
Andy Fang <andy@mingtak.com.tw>
2017-02-17 07:25:07 -0700

    update interfaces.py

--- src/mingjing/content/interfaces.py
+++ src/mingjing/content/interfaces.py
@@ -5,6 +5,15 @@ from mingjing.content import _
 from zope import schema
 from zope.interface import Interface
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from z3c.relationfield.schema import RelationChoice, RelationList
+from plone.app.vocabularies.catalog import CatalogSource
+from plone.supermodel import model
+from DateTime import DateTime
+
+date_range = {
+    'query': (DateTime()-1, DateTime()), 
+    'range': 'min:max',
+}
 
 
 class IMingjingContentLayer(IDefaultBrowserLayer):
@@ -28,11 +37,41 @@ class IMember(Interface):
     """  """
 
 
-
 class ICover(Interface):
     """  """
 
+    mainSliderNews = schema.Text(
+        title=_(u"Main Slider News"),
+        description=_(u"Please input news id, per line one id, sorted on modified, max 10 news."),
+        required=False,
+    )
+
+    todayNews = schema.Text(
+        title=_(u"Today news"),
+        description=_(u"Please input news id, per line one id, sorted on modified, max 8 news."),
+        required=False,
+    )
+
+    tabsName = schema.Text(
+        title=_(u"Tabs Name"),
+        description=_(u"Please input tabs name, per line 5 tabs, Separated by commas, max 3 lines."),
+        required=False,
+    )
+
+    ebooks = schema.Text(
+        title=_(u"Ebooks Introduction"),
+        description=_(u"Please input books id, per line 1 id, In order is Newest, Hotest and eBook."),
+        required=False,
+    )
+
+    rankingNews = schema.Text(
+        title=_(u"Ranking News"),
+        description=_(u"Please input ranking news id, per line one id, sorted on modified, max 6 news."),
+        required=False,
+    )
+
     radioList = schema.Text(
         title=_(u"Radio List"),
+        description=_(u"Don't edit this, auto renew."),
         required=False,
     )
